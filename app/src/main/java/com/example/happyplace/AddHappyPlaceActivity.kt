1836b926d428a321d3250e85ef2464b191113bea
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2022-11-08 01:03:30 +0530

    Dexter implementation for app permission management

--- app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplace/AddHappyPlaceActivity.kt
@@ -2,11 +2,22 @@ package com.example.happyplace
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.example.happyplace.databinding.ActivityAddHappyPlaceBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -66,9 +77,9 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 pictureDialog.setItems(pictureDialogItems){
                     _, which ->
                     when(which){
-                        0 ->  {Toast.makeText(this@AddHappyPlaceActivity, "You are useing the Gallery", Toast.LENGTH_LONG).show()
-                            //choosePhotoFromGallery()
-                        }
+                        0 ->
+                            choosePhotoFromGallery()
+
                         1 -> Toast.makeText(this@AddHappyPlaceActivity, "You are useing the camera", Toast.LENGTH_LONG).show()
                     }
                 }
@@ -83,9 +94,44 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
         binding?.etDate?.setText(sdf.format(cal.time).toString())
     }
 
-    private fun choosePhotoFromGallery(){
-
+    private fun showRationalDialogforPermission(){
+        AlertDialog.Builder(this)
+            .setMessage("You dont grant permission which are required by this app")
+            .setPositiveButton("Go To Settings"){
+                _, _ ->
+                try {
+                    //If user want to enable the permission then use intent to go to the Settings
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }catch (e: ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){
+                dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
+
+    private fun choosePhotoFromGallery(){
+        Dexter.withContext(this)
+            .withPermissions(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            .withListener(object : MultiplePermissionsListener {
+                override  fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()){ //If all the permission is granted by user
+                        Toast.makeText(this@AddHappyPlaceActivity, "All Permission Granted so you have to select image from Gallery", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onPermissionRationaleShouldBeShown(permissions: MutableList<PermissionRequest>, token: PermissionToken) {
+                    //If the permission is not granted by the user
+                    showRationalDialogforPermission()
+                }
+            }).onSameThread().check()
+    }
+
+
 
     override fun onDestroy() {
         super.onDestroy()
